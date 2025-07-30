9764648fa834e7ed9ada194529fe4e9ef9dd6f20
ArtemUshakov256 <ushakov.artem256@mail.ru>
2025-07-30 17:07:34 +0300

    make tips and additional angle table

--- app/presentation/ui/main_window.py
+++ app/presentation/ui/main_window.py
@@ -1,10 +1,12 @@
 
 from PySide6.QtWidgets import (
+    QScrollArea, QSizePolicy, QAbstractScrollArea,
     QApplication, QWidget, QVBoxLayout, QLabel, QTableWidget,
     QTableWidgetItem, QPushButton, QComboBox, QHeaderView
 )
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QBrush, QColor
+from ..constants import *
 import json
 
 
@@ -12,36 +14,55 @@ class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Amast KMD")
-        self.resize(1600, 800)
+        self.setMinimumSize(1000, 600)
+        self.adjustSize()
         layout = QVBoxLayout(self)
 
         layout.addWidget(QLabel("Секции"))
         self.sections_table = self.create_sections_table()
         self.set_initial_state_in_sections_table()
-        layout.addWidget(self.sections_table)
+        scroll1 = QScrollArea()
+        scroll1.setWidgetResizable(True)
+        self.sections_table.setSizeAdjustPolicy(QAbstractScrollArea.AdjustToContents)
+        self.sections_table.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        scroll1.setWidget(self.sections_table)
+        layout.addWidget(scroll1)
 
         layout.addWidget(QLabel("Траверсы"))
         self.traverse_table = self.create_traverse_table()
         self.set_initial_state_in_traverse_table()
-        layout.addWidget(self.traverse_table)
+        scroll2 = QScrollArea()
+        scroll2.setWidgetResizable(True)
+        self.traverse_table.setSizeAdjustPolicy(QAbstractScrollArea.AdjustToContents)
+        self.traverse_table.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        scroll2.setWidget(self.traverse_table)
+        layout.addWidget(scroll2)
+
+        layout.addWidget(QLabel("Угол узла на уровне троса"))
+        self.additional_table = self.create_additional_table()
+        scroll3 = QScrollArea()
+        scroll3.setWidgetResizable(True)
+        self.additional_table.setSizeAdjustPolicy(QAbstractScrollArea.AdjustToContents)
+        self.additional_table.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        scroll3.setWidget(self.additional_table)
+        layout.addWidget(scroll3)
 
         self.import_button = QPushButton("Экспортировать данные")
         self.import_button.clicked.connect(self.import_data)
         layout.addWidget(self.import_button)
 
     def create_sections_table(self):
-        self.sections_headers = [
-            "Секции", "Тип соединения с\nнижней секцией", "Lсекции, мм", "Толщина секции, мм",
-            "Dсекции, мм", "Толщина фланца, мм", "Кол-во болтов, шт", "d болтов, мм",
-            "Длина стыка\nтелескопа, мм", "Кол-во скорлупок\nсекции, шт", "D располож.\nболтов, мм",
-        ]
-        self.sections_rows = ["A-нижняя", "", "B", "C", "D", "E", "Свая"] # "" - необходимо для объединения первых строк
-        table = QTableWidget(len(self.sections_rows) + 1, len(self.sections_headers))
+        table = QTableWidget(len(SECTIONS_ROWS) + 1, len(SECTIONS_HEADERS))
         table.verticalHeader().setVisible(False)
-        table.setHorizontalHeaderLabels(self.sections_headers)
+        
+        self.set_headers_with_optional_tooltips(
+            table,
+            SECTIONS_HEADERS,
+            SECTIONS_TIPS_DICT
+        )
 
-        for row_idx, row_name in enumerate(self.sections_rows):
-            for col_idx, col_name in enumerate(self.sections_headers):
+        for row_idx, row_name in enumerate(SECTIONS_ROWS):
+            for col_idx, col_name in enumerate(SECTIONS_HEADERS):
                 if row_idx == 0 and col_idx != 4:
                     table.setSpan(row_idx, col_idx, 2, 1)
                 if col_idx == 0:
@@ -89,20 +110,12 @@ class MainWindow(QWidget):
         return table
 
     def create_traverse_table(self):
-        self.headers_top = [
-            "Траверсы", "Высота", "Узел в\nствол опоры", "Угол поворота\nтраверсы, град",
-            "Длина Л, мм", "Длина П, мм", "Диаметр D, мм", "Диаметр d, мм", "Толщина тр., мм",
-            "Толщина ванг/\nскоб, мм", "Кол-во болтов, шт", "Диам. болтов, мм", "Мин. расст.\nмежду вангами",
-            "Узел крепления Траверса Л", "Узел крепления Траверса П",
-            "Положение узла Траверса Л", "Положение узла Траверса П", "Пластина на\nконце траверсы"
-        ]
-        self.traverse_rows = ["1", "2", "3", "4", "5", "6", "Трос.тр."]
-
-        row_count = 2 + len(self.traverse_rows)  # 2 заголовка + данные
+        row_count = 2 + len(TRAVERSE_ROWS)  # 2 заголовка + данные
         col_count = 34  # колонки разбиты вручную
 
         table = QTableWidget(row_count, col_count)
-        table.setHorizontalHeaderLabels(self.headers_top[:-5])
+        # table.setHorizontalHeaderLabels(HEADERS_TOP[:-5])
+        self.set_headers_with_optional_tooltips(table, HEADERS_TOP[:-5], TRAVERSE_TIPS_DICT)
         table.verticalHeader().setVisible(False)
         table.horizontalHeader().setVisible(False)
         table.resizeColumnsToContents()
@@ -110,58 +123,46 @@ class MainWindow(QWidget):
 
         # Верхние заголовки (первая строка)
         top_row = 0
-        combobox_list = ["Узел в\nствол опоры", "Угол поворота\nтраверсы, град"]
-        for col, header in enumerate(self.headers_top):
+        combobox_list = ["Узел в\nствол опоры (ℹ)", "Угол поворота\nтраверсы, град"]
+        for col, header in enumerate(HEADERS_TOP):
             if header in combobox_list:
                 for row in range(2, 9):
                     combo = QComboBox()
-                    if header == "Узел в\nствол опоры":
+                    if header == "Узел в\nствол опоры (ℹ)":
                         combo.addItems(["0", "1", "2", "3", "4", "5"])
+                        self.set_item_with_optional_tooltip(table, row, col, header, TRAVERSE_TIPS_DICT)
                     else:
                         combo.addItems(["0", "30", "60", "-30", "60"])
                     table.setCellWidget(row, col, combo)
                     item = table.item(row, col)
-            if header == "Узел крепления Траверса Л":
+            if header == "Узел крепл. Траверса Л (ℹ)":
                 table.setSpan(top_row, 13, 1, 5)
-                item = QTableWidgetItem(header)
-                item.setFlags(Qt.ItemIsEnabled)
-                item.setTextAlignment(Qt.AlignCenter)
-                table.setItem(top_row, 13, item)
-            elif header == "Узел крепления Траверса П":
+                self.set_item_with_optional_tooltip(table, top_row, 13, header, TRAVERSE_TIPS_DICT)
+            elif header == "Узел крепл. Траверса П":
                 table.setSpan(top_row, 18, 1, 5)
                 item = QTableWidgetItem(header)
                 item.setFlags(Qt.ItemIsEnabled)
                 item.setTextAlignment(Qt.AlignCenter)
                 table.setItem(top_row, 18, item)
-            elif header == "Положение узла Траверса Л":
+            elif header == "Распол. узла Траверса Л (ℹ)":
                 table.setSpan(top_row, 23, 1, 5)
-                item = QTableWidgetItem(header)
-                item.setFlags(Qt.ItemIsEnabled)
-                item.setTextAlignment(Qt.AlignCenter)
-                table.setItem(top_row, 23, item)
-            elif header == "Положение узла Траверса П":
+                self.set_item_with_optional_tooltip(table, top_row, 23, header, TRAVERSE_TIPS_DICT)
+            elif header == "Распол. узла Траверса П":
                 table.setSpan(top_row, 28, 1, 5)
                 item = QTableWidgetItem(header)
                 item.setFlags(Qt.ItemIsEnabled)
                 item.setTextAlignment(Qt.AlignCenter)
                 table.setItem(top_row, 28, item)
-            elif header == "Пластина на\nконце траверсы":
+            elif header == "Пластина на\nконце траверсы (ℹ)":
                 table.setSpan(top_row, 33, 2, 1)
-                item = QTableWidgetItem(header)
-                item.setFlags(Qt.ItemIsEnabled)
-                item.setTextAlignment(Qt.AlignCenter)
-                table.setItem(top_row, 33, item)
+                self.set_item_with_optional_tooltip(table, top_row, 33, header, TRAVERSE_TIPS_DICT)
             else:
                 table.setSpan(top_row, col, 2, 1)
-                item = QTableWidgetItem(header)
-                item.setFlags(Qt.ItemIsEnabled)
-                item.setTextAlignment(Qt.AlignCenter)
-                table.setItem(top_row, col, item)
+                self.set_item_with_optional_tooltip(table, top_row, col, header, TRAVERSE_TIPS_DICT)
         # Вложенные заголовки
-        subheaders = ["№1", "№2", "№3", "№4", "№5"]
         col_start = 13
         for i in range(4):
-            for subheader, name in enumerate(subheaders):
+            for subheader, name in enumerate(SUBHEADERS):
                 item = QTableWidgetItem(name)
                 item.setFlags(Qt.ItemIsEnabled)
                 item.setTextAlignment(Qt.AlignCenter)
@@ -177,7 +178,7 @@ class MainWindow(QWidget):
         table.setCellWidget(2, 33, combo)
 
         # Основные строки данных
-        for row_idx, row_name in enumerate(self.traverse_rows):
+        for row_idx, row_name in enumerate(TRAVERSE_ROWS):
             item = QTableWidgetItem(row_name)
             item.setTextAlignment(Qt.AlignCenter)
             table.setItem(row_idx + 2, 0, item)
@@ -188,6 +189,25 @@ class MainWindow(QWidget):
                 
         return table
 
+    def create_additional_table(self, ):
+        table = QTableWidget(4, 2)
+        table.verticalHeader().setVisible(False)
+        table.horizontalHeader().setVisible(False)
+        table.setSpan(0, 0, 4, 1)
+        self.set_item_with_optional_tooltip(
+            table,
+            0,
+            0,
+            "Угол узла\nв ствол на\nуровне\nтрос.трав.",
+        )
+
+        for i in range(4):
+            combo = QComboBox()
+            combo.addItems(DEGREE_COMBOBOX)
+            table.setCellWidget(i, 1, combo)
+
+        return table
+    
     def handle_connection_type_change(self, row_idx):
         table = self.sections_table
         conn_type = table.cellWidget(row_idx, 1).currentText()
@@ -196,12 +216,12 @@ class MainWindow(QWidget):
         if conn_type == "Фланц.":
             disabled_cols = ["Длина стыка\nтелескопа, мм"]
         elif conn_type == "Телескоп.":
-            disabled_cols = ["Dсекции, мм", "Толщина фланца, мм",
+            disabled_cols = ["Dсекции, мм (ℹ)", "Толщина фланца, мм",
                              "Кол-во болтов, шт", "d болтов, мм",
                              "D располож.\nболтов, мм"]
 
         for col_idx in range(2, table.columnCount()):
-            col_name = self.sections_headers[col_idx]
+            col_name = SECTIONS_HEADERS[col_idx]
             item = table.item(row_idx, col_idx)
             if col_name in disabled_cols and col_name in ["Кол-во болтов, шт", "d болтов, мм"]:
                 combo = table.cellWidget(row_idx, col_idx)
@@ -219,7 +239,7 @@ class MainWindow(QWidget):
 
     def set_initial_state_in_sections_table(self):
         table = self.sections_table
-        for row_idx in range(len(self.sections_rows)):
+        for row_idx in range(len(SECTIONS_ROWS)):
             if row_idx == 6:
                 for col in [1, 4, 5, 6, 7, 8, 10]:
                     self.disable_and_grey_cell(table, row_idx, col)
@@ -228,7 +248,7 @@ class MainWindow(QWidget):
 
     def set_initial_state_in_traverse_table(self):
         table = self.traverse_table
-        for row_idx in range(len(self.traverse_rows) + 2):
+        for row_idx in range(len(TRAVERSE_ROWS) + 2):
             if row_idx > 4:
                 for col in [15, 16, 17, 20, 21, 22, 25, 26, 27, 30, 31, 32]:
                     table.removeCellWidget(row_idx, col)
@@ -246,13 +266,45 @@ class MainWindow(QWidget):
         item.setBackground(QBrush(QColor("grey")))
         item.setText("")
 
+    def set_headers_with_optional_tooltips(self, table, headers: list[str], tooltips: dict[str, str]):
+        """
+        Устанавливает заголовки таблицы. К заголовкам, указанным в словаре tooltips, добавляет всплывающую подсказку.
+        
+        :param table: QTableWidget
+        :param headers: Список заголовков
+        :param tooltips: Словарь {заголовок: текст подсказки}
+        """
+        for i, title in enumerate(headers):
+            item = QTableWidgetItem(title)
+            if title in tooltips:
+                item.setToolTip(tooltips[title])
+            table.setHorizontalHeaderItem(i, item)
+
+    def set_item_with_optional_tooltip(self, table, row: int, col: int, value: str, tooltips: dict[tuple[int, int], str] | None = None):
+        """
+        Устанавливает значение ячейки и добавляет всплывающую подсказку, если она есть в словаре.
+
+        :param table: QTableWidget
+        :param row: Индекс строки
+        :param col: Индекс столбца
+        :param value: Текст для отображения в ячейке
+        :param tooltips: Словарь {(row, col): tooltip}
+        """
+        item = QTableWidgetItem(value)
+        item.setFlags(Qt.ItemIsEnabled)
+        item.setTextAlignment(Qt.AlignCenter)
+        key = (row, col)
+        if tooltips and key in tooltips:
+            item.setToolTip(tooltips[key])
+        table.setItem(row, col, item)
+
     def import_data(self):
         section_data = {}
         for row in range(self.sections_table.rowCount()):
             key = self.sections_table.item(row, 0).text()
             section_data[key] = {}
             for col in range(1, self.sections_table.columnCount()):
-                header = self.sections_headers[col]
+                header = SECTIONS_HEADERS[col]
                 if self.sections_table.cellWidget(row, col):
                     val = self.sections_table.cellWidget(row, col).currentText()
                 else:
